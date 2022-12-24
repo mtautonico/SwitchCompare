786de0e0ed7b7bc89ace9b582a8aae320d9161fd
Michael Tautonico <mario6297@gmail.com>
2022-12-24 02:15:54 -0500

    Filters function like filters, next is making the site dark theme

--- client/src/app/pages/brand/brand.component.ts
+++ client/src/app/pages/brand/brand.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {APIFetchService} from "../../services/apifetch/apifetch.service";
 import {TabletoolsService} from "../../services/tabletools/tabletools.service";
+import {createCssResourcePlugin} from "@angular-devkit/build-angular/src/builders/browser-esbuild/css-resource-plugin";
 
 interface Switch {
   brand: string;
@@ -27,6 +28,7 @@ export class BrandComponent {
   currentSortDirectionIsAsc: boolean = true;
   currentSortField: string = 'model';
   lastSortField: string = 'model';
+  filtered_switches: Switch[] = [];
   // These are for the table header sorting
   headerSort: any = {
     brand: null,
@@ -62,7 +64,7 @@ export class BrandComponent {
     }
   }
   // idk I felt like being slightly efficient in my code for once
-  range_filters: any = ['actuation_distance', 'bottom_distance', 'operating_force', 'bottom_force'];
+  range_filters: any = ['actuation_distance', 'bottom_distance', 'bottom_force', 'operating_force'];
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -82,7 +84,7 @@ export class BrandComponent {
       this.headerSort[this.lastSortField] = null;
       this.headerSort[key] = "asc";
       this.lastSortField = key;
-    //  If the interacted field is the same as the last one, reverse the sort direction
+      //  If the interacted field is the same as the last one, reverse the sort direction
     } else {
       this.switches = this.TabletoolsService.sortJSON(data, key, !this.currentSortDirectionIsAsc);
       if (this.currentSortDirectionIsAsc) {
@@ -103,19 +105,74 @@ export class BrandComponent {
 
   // Sets the filters back to default (all False and null basically)
   resetFilters() {
+    // Nulls all range filters
     for (let i = 0; i < this.range_filters.length; i++) {
       this.filters[this.range_filters[i]].min = null;
       this.filters[this.range_filters[i]].max = null;
     }
+    // Set the type filters to false
     for (let i = 0; i < Object.keys(this.filters.type).length; i++) {
       this.filters.type[Object.keys(this.filters.type)[i]] = false;
     }
+    // Reset the filtered switches
+    this.filtered_switches = [];
   }
 
-  // TODO: Make a function to filter through the data and set said filtered data to a separate variable
   filter_switches() {
-
+    // Reset the filtered switches
+    this.filtered_switches = [];
+    // Checklist that each switch has to pass to be added to the filtered switches
+    let filteredChecklist = {
+      type: false,
+      actuation_distance: false,
+      bottom_distance: false,
+      operating_force: false,
+      bottom_force: false
+    }
+    let filteredTypes = [];
+    // Add all the selected switch types to a list of types to filter by
+    for (let i = 0; i < Object.keys(this.filters.type).length; i++) {
+      if (this.filters.type[Object.keys(this.filters.type)[i]]) {
+        filteredTypes.push(Object.keys(this.filters.type)[i]);
+      }
+    }
+    for (let i = 0; i < this.switches.length; i++) {
+      // Reset the checklist
+      filteredChecklist = {
+        type: false,
+        actuation_distance: false,
+        bottom_distance: false,
+        operating_force: false,
+        bottom_force: false
+      }
+      // If the switch type is in the filtered types, set the type filter to true
+      if (filteredTypes.includes(this.switches[i].type)) {
+        filteredChecklist.type = true;
+      }
+      // Loop through each range filter to determine if the switch is within the range or if a range is given
+      for (let j = 0; j < this.range_filters.length; j++) {
+        let current_range: string = this.range_filters[j].toString();
+        if (this.filters[current_range].min !== null && this.filters[current_range].max !== null) {
+          // @ts-ignore
+          if (this.switches[i][current_range] >= this.filters[current_range].min &&
+            // @ts-ignore
+            this.switches[i][current_range] <= this.filters[current_range].max) {
+            // @ts-ignore
+            filteredChecklist[current_range] = true;
+          }
+        } else if (this.filters[current_range].min == null && this.filters[current_range].max == null) {
+          // @ts-ignore
+          filteredChecklist[current_range] = true;
+        }
+      }
+      // If the switch passes all the filters, add it to the filtered switches
+      if (filteredChecklist.type && filteredChecklist.actuation_distance && filteredChecklist.bottom_distance &&
+        filteredChecklist.operating_force && filteredChecklist.bottom_force) {
+        this.filtered_switches.push(this.switches[i]);
+      }
+    }
   }
+
 
   // We need the brand in case theres a situation where 2 brands have the same model name
   // TODO: Write this function
@@ -124,6 +181,7 @@ export class BrandComponent {
   }
 
   // These run when the page is loaded
+
   async ngOnInit() {
     // Gets params from the URL
     this.route.paramMap.subscribe(params => {
@@ -137,11 +195,11 @@ export class BrandComponent {
       this.switches = this.switches.flat();
       console.log(this.switches)
       console.log("triggered a")
-    //  If there are no selected switches, get all switches from the brand
+      //  If there are no selected switches, get all switches from the brand
     } else if (this.brand == undefined) {
       this.switches = await this.APIFetchService.getSwitches();
       console.log("triggered b")
-    //  If there is a single brand selected, get all switches from that brand
+      //  If there is a single brand selected, get all switches from that brand
     } else {
       this.switches = await this.APIFetchService.getSwitches(this.brand);
       console.log("triggered c")
